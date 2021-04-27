46001c4b3cac174096d40a97a41b836bb5ef09ea
TatjanaBecic <becictatjanas1a@gmail.com>
2021-04-27 07:13:16 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'ucenik');
+  define('DB_PASS', '1234');
+  define('DB_NAME', 'ucenik');
+
+  
+  define('APPROOT', dirname(dirname(__FILE__)));
+ 
+  define('URLROOT', 'http://37.0.71.52/s4f.test/becic.tatjana/shareposts');
+  
+  define('SITENAME', 'Projekat');
+  
+  define('APPVERSION', '1.0.0');
